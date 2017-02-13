32fbfb27f4d8063d806ee4dc951558382b13425f
danielglennross <danielglennross@gmail.com>
2017-02-13 16:23:44 +0000

    added eslint

--- restService.js
+++ restService.js
@@ -6,15 +6,20 @@ const Wreck = require('wreck');
 
 const schemaTypes = ['headers', 'params', 'query', 'payload'];
 
-module.exports = (apiSchema, baseUri, settings = {}) => {
+module.exports = (apiSchema, baseUriFac /* , settings = {}*/) => {
 
-  Hoek.assert(['string', 'function'].some(type => typeof baseUri === type), '"basUri" must be a string or function');
+  Hoek.assert(
+    ['string', 'function'].some(type => typeof baseUriFac === type),
+    '"basUri" must be a string or function'
+  );
 
-  if (typeof baseUri === 'function') {
-    baseUri = baseUri();
-  }
+  const baseUri = typeof baseUriFac === 'function'
+    ? baseUriFac()
+    : baseUriFac;
 
-  const httpRequest = (method, url, options) => {
+  const httpRequest = (method, url, requestOptions) => {
+    const options = requestOptions;
+
     options.headers = Object.assign({}, options.headers || {}, {
       'Content-Type': 'application/json',
       Accept: 'application/json'
@@ -29,8 +34,10 @@ module.exports = (apiSchema, baseUri, settings = {}) => {
         if (err) {
           return reject(err);
         }
-        res.payload = payload instanceof Buffer 
-          ? JSON.parse(payload.toString() || "{}") 
+
+        // eslint-disable-next-line no-param-reassign
+        res.payload = payload instanceof Buffer
+          ? JSON.parse(payload.toString() || '{}')
           : payload;
         return resolve(res);
       })
@@ -38,33 +45,35 @@ module.exports = (apiSchema, baseUri, settings = {}) => {
   };
 
   const createApiCall = (schema) => {
-    
+
     // ensure correct joi format
-    cleanSchema = Hoek.clone(schema).forEach(type => {
-      if (schemaTypes.includes(type) && 
-          schema[type] && 
-          !schema[type].isJoi) {
-        schema[type] = Joi.compile(schema[type]);
+    const cleanSchema = Hoek.clone(schema);
+    cleanSchema.forEach(type => {
+      if (schemaTypes.includes(type) &&
+            schema[type] &&
+              !schema[type].isJoi) {
+        cleanSchema[type] = Joi.compile(schema[type]);
       }
     });
-    
+
     return (args) => {
 
       // map args with schema
       const options = Object.keys(args).reduce((agg, k) => {
         const clone = [...schemaTypes];
         const create = (type) => {
-          if (!newType) {
+          if (!type) {
             return;
           }
-
-          const item = schema[type] && schema[type]._inner.children.find(x => x.key.toLowerCase() === k.toLowerCase());
+          const item = schema[type] && schema[type]._inner.children.find(
+            x => x.key.toLowerCase() === k.toLowerCase()
+          );
           if (item) {
+            // eslint-disable-next-line no-param-reassign
             agg[type] = Object.assign({}, agg[type] || {}, { [k]: args[k] });
             return;
           }
-
-          return create(clone.shift());
+          create(clone.shift());
         };
         create(clone.shift());
         return agg;
@@ -75,13 +84,12 @@ module.exports = (apiSchema, baseUri, settings = {}) => {
 
       const validateFactory = (type) => {
         if (options && options[type]) {
-          return new Promise((resolve, reject) => {
-            return Joi.validate(options[type], schema[type], (err, val) => {
-              if (err) return reject(err);
-              resolve(val);
-            });
-          });
-        } 
+          return new Promise((resolve, reject) =>
+            Joi.validate(options[type], schema[type], (err, val) =>
+              (err ? reject(err) : resolve(val))
+            )
+          );
+        }
         return Promise.resolve();
       };
 
@@ -92,24 +100,25 @@ module.exports = (apiSchema, baseUri, settings = {}) => {
       ]) => {
         if (params) {
           Object.keys(options.params).forEach(key => {
-            schema.path = schema.path.replace(`{${key}}`, encodeURIComponent(options.params[key]));
+            cleanSchema.path = cleanSchema.path.replace(`{${key}}`, encodeURIComponent(options.params[key]));
           });
         }
         if (query) {
-          const keyVals = Object.keys(options.query).map(key => 
+          const keyVals = Object.keys(options.query).map(key =>
             `${key}=${encodeURIComponent(options.query[key])}`
           );
-          schema.path += `?${keyVals.join('&')}`;
+          cleanSchema.path += `?${keyVals.join('&')}`;
         }
 
-        const method = schema.method.toLowerCase();
-        const url = `${baseUri.trim('/')}${schema.path}`;
-        return httpRequest(method, url, { headers, payload })
+        const method = cleanSchema.method.toLowerCase();
+        const url = `${baseUri.trim('/')}${cleanSchema.path}`;
+        return httpRequest(method, url, { headers, payload });
       });
-    }
+    };
   };
 
   const endpoints = Object.keys(apiSchema).reduce((api, key) => {
+    // eslint-disable-next-line no-param-reassign
     api[key] = createApiCall(apiSchema[key]);
     return api;
   }, {});
